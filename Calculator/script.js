b54a79552451816bfde855c00d99e4a106f77599
faheem2 <muhammad.faheem.khan71@gmail.com>
2022-11-15 20:03:18 +0500

    Added Simple Calculator Project

---
+++ Calculator/script.js
@@ -0,0 +1,22 @@
+
+// display 
+let display = document.getElementById('display')
+
+function displayHandler(e) {
+    display.value += e.target.value
+}
+function allClearHandler() {
+    display.value = ''
+}
+function clearHandler(){
+    display.value = display.value.slice(0,-1)
+}
+function equalHandler() {
+    try {
+        if (display.value) {
+            display.value = eval(display.value)
+        }
+    } catch (error) {
+        display.value = 'Error: Invalid Entry'
+    }
+}
